f36ce4499c6b9f50d3f3a15b742144d72ffd98fb
SpencerMueller <muellers@mail.gvsu.edu>
2021-06-19 16:38:16 -0400

    detail screen progress

--- src/screens/PlaceDetailScreen.tsx
+++ src/screens/PlaceDetailScreen.tsx
@@ -1,17 +1,38 @@
 import React, { FunctionComponent, useEffect } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { Button } from 'react-native-elements';
+import { TouchableOpacity } from 'react-native-gesture-handler';
 import { PlaceDetailScreenNavigationProp } from '../types';
+import { MaterialIcons } from '@expo/vector-icons'; 
+import { Input } from 'react-native-elements/dist/input/Input';
 
 const PlaceDetailScreen: FunctionComponent<PlaceDetailScreenNavigationProp> = ({ route, navigation }) => {
 	const placeInfo = route.params.placeInfo;
 
 	useEffect(() => {
-		navigation.setOptions({ headerTitle: placeInfo.name });
+		navigation.setOptions({ headerTitle: placeInfo.name,
+			headerRight: () => (
+				<TouchableOpacity
+					onPress={() =>
+						navigation.navigate('BottomTabs')
+					}
+				>
+					<MaterialIcons name="favorite-outline" size={24} color="white" style={{marginRight: 10}} />
+				</TouchableOpacity>
+			  )
+		});
 	}, []);
+
+	var overallrating = 5
+	var userrating = 1
 
 	return (
 		<View style={styles.container}>
-			<Text>github</Text>
+			<Text style={styles.text}>{JSON.stringify(placeInfo.name)}</Text>
+			<Text style={styles.text}>Overall Rating: {overallrating}</Text>
+			<Text style={styles.text}>User Rating: {userrating}</Text>
+			<Input style={styles.rating} placeholder={'Enter a Rating'}></Input>
+			<Button title= 'Submit'></Button>
 			<Text>{JSON.stringify(placeInfo)}</Text>
 		</View>
 	);
@@ -23,6 +44,15 @@ const styles = StyleSheet.create({
 		backgroundColor: '#fff',
 		alignItems: 'center',
 		justifyContent: 'center'
+	},
+	text: {
+		marginTop: 20,
+		textAlign: 'center',
+		fontSize: 35,
+		fontWeight: 'bold'
+	},
+	rating: {
+		
 	}
 });
 
