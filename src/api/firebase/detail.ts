cbbcb3daa012e6719c2677264d22711cd56d597b
Spencer Mueller <38333791+SpencerMueller@users.noreply.github.com>
2021-06-20 20:29:05 -0400

    firebase

---
+++ src/api/firebase/detail.ts
@@ -0,0 +1,39 @@
+import 'firebase/database';
+import 'firebase/firestore';
+import { fb } from './login';
+
+const USERS = 'users';
+const RATINGS ='ratings';
+const USERS_COLLECTION = fb.firestore().collection(USERS);
+const RATINGS_COLLECTION = fb.firestore().collection(RATINGS)
+			
+
+export const addUserRating = async (userID: string | null, userRating: number, breweryName: string) => {
+    if (!userID) return () => {};
+    USERS_COLLECTION.doc(userID).collection("ratings").doc(breweryName).set({
+        userRating: userRating
+    })
+    .then(() => {
+        console.log("Document successfully written!");
+    })
+    .catch((error: any) => {
+        console.error("Error writing document: ", error);
+    });
+}
+
+export const addOverallRating = async (userID: string | null, myRating: number, breweryName: string) => {
+    if (!userID) return () => {};
+    var overallRating = 5
+
+    console.log("whis is this " + breweryName)
+
+    RATINGS_COLLECTION.doc(breweryName).set({
+        rating: overallRating
+    })
+    .then(() => {
+        console.log("Document successfully written 2!");
+    })
+    .catch((error: any) => {
+        console.error("Error writing document: ", error);
+    });
+}
